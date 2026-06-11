55cdd5b2e81d0c81a86327baf925156644bb8d06
coneglitch <khanhtranvan392@gmail.com>
2026-06-11 19:24:16 +0700

    Add files via upload

--- Locket_DuyVinh09.js
+++ Locket_DuyVinh09.js
@@ -1,45 +1,69 @@
-// ========= ID ========= //
-const mapping = {
-  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-  'Locket': ['Gold']
-};
-// =========   Phần cố định  ========= // 
-// =========  @duyvinh09 ========= // 
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
+// ========= Locket Gold Unlock ========= //
+// Fix: Khong phu thuoc User-Agent, inject Gold truc tiep vao tat ca entitlements
+// @author: duyvinh09 | Fix by Antigravity
+
+var obj;
+try {
   obj = JSON.parse($response.body);
-obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
-var duyvinh09 = {
-      auto_resume_date: null,
-      display_name: "locket_1600_1y",
-      is_sandbox: true,
-      ownership_type: "PURCHASED",
-      billing_issues_detected_at: null,
-      management_url: "https://apps.apple.com/account/subscriptions",
-      period_type: "normal",
-      price: {
-          "amount": 399000.0,
-          "currency": "VND"
-      },
-      expires_date: "9999-01-09T10:10:14Z",
-      grace_period_expires_date: null,
-      refunded_at: null,
-      unsubscribe_detected_at: null,
-      original_purchase_date: "2005-01-09T10:10:15Z",
-      purchase_date: "2005-01-09T10:10:14Z",
-      store: "app_store",
-      store_transaction_id: "2000001108724193",
+} catch (e) {
+  $done({});
+}
+
+// Subscription object gia lap - Gold vinh vien
+var fakeSubscription = {
+  auto_resume_date: null,
+  display_name: "locket_1600_1y",
+  is_sandbox: false,
+  ownership_type: "PURCHASED",
+  billing_issues_detected_at: null,
+  management_url: "https://apps.apple.com/account/subscriptions",
+  period_type: "normal",
+  price: {
+    amount: 399000.0,
+    currency: "VND"
   },
-  locketGold = {
-      grace_period_expires_date: null,
-      purchase_date: "2005-01-09T10:10:14Z",
-      product_identifier: "locket_1600_1y",
-      expires_date: "9999-01-09T10:10:14Z"
-  };
-const match = Object.keys(mapping).find(e => ua.includes(e));
-if (match) {
-  let [e, s] = mapping[match];
-  s ? (locketGold.product_identifier = s, obj.subscriber.subscriptions[s] = duyvinh09) : obj.subscriber.subscriptions["locket_1600_1y"] = duyvinh09, obj.subscriber.entitlements[e] = locketGold
-} else obj.subscriber.subscriptions["locket_1600_1y"] = duyvinh09, obj.subscriber.entitlements.pro = locketGold;
+  expires_date: "2099-01-09T10:10:14Z",
+  grace_period_expires_date: null,
+  refunded_at: null,
+  unsubscribe_detected_at: null,
+  original_purchase_date: "2020-01-09T10:10:15Z",
+  purchase_date: "2020-01-09T10:10:14Z",
+  store: "app_store",
+  store_transaction_id: "2000001108724193"
+};
+
+// Entitlement object cho Gold
+var fakeEntitlement = {
+  grace_period_expires_date: null,
+  purchase_date: "2020-01-09T10:10:14Z",
+  product_identifier: "locket_1600_1y",
+  expires_date: "2099-01-09T10:10:14Z"
+};
+
+// Kiem tra subscriber ton tai
+if (obj && obj.subscriber) {
+  var sub = obj.subscriber;
+
+  // Khoi tao neu thieu
+  if (!sub.subscriptions) sub.subscriptions = {};
+  if (!sub.entitlements) sub.entitlements = {};
+  if (!sub.non_subscriptions) sub.non_subscriptions = {};
+
+  // Inject subscription chinh
+  sub.subscriptions["locket_1600_1y"] = fakeSubscription;
+
+  // Inject tat ca entitlement co the Locket dung: pro, Gold, vip, watch_vip
+  var entitlementKeys = ["pro", "Gold", "gold", "vip", "watch_vip", "locket_gold"];
+  entitlementKeys.forEach(function(key) {
+    sub.entitlements[key] = fakeEntitlement;
+  });
+
+  // Patch first_seen de tranh bi detect la moi
+  if (!sub.first_seen) {
+    sub.first_seen = "2020-01-09T10:10:14Z";
+  }
+}
+
 $done({
   body: JSON.stringify(obj)
 });
--- deleteHeader.js
+++ deleteHeader.js
@@ -1,8 +1,14 @@
-/***********************************************
-> deleteHeader by duyvinh09
-***********************************************/	
+// deleteHeader by duyvinh09 | Fix: xoa ca 2 dang header ETag de RevenueCat luon gui full response
 
-const version = 'V1.0.2';
+var modifiedHeaders = $request.headers;
 
+// Xoa het cac bien the cua ETag header (case-insensitive safe)
+var keysToDelete = ["X-RevenueCat-ETag", "x-revenuecat-etag", "If-None-Match", "if-none-match"];
+keysToDelete.forEach(function(key) {
+  // Xoa ca dang chinh xac va lowercase
+  delete modifiedHeaders[key];
+  delete modifiedHeaders[key.toLowerCase()];
+  delete modifiedHeaders[key.toUpperCase()];
+});
 
-function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
+$done({ headers: modifiedHeaders });
